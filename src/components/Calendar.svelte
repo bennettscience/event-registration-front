bb4877aef859d771a86e14efc94511d3b371db8b
bennettscience <brian.bennett2@gmail.com>
2021-08-05 08:03:07 -0400

    adjust center alignment

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -20,6 +20,7 @@
         display: flex;
         flex-direction: column;
         justify-content: center;
+        align-items: center;
         box-sizing: border-box;
         height: 100%;
         padding: 10px;
